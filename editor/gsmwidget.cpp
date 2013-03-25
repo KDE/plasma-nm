bf333b53f5f2a5106964e0d32ed16fc91b88e5b1
Lukáš Tinkl <lukas@kde.org>
2013-03-25 17:30:04 +0100

    implemented GSM setting widget
    not used yet

---
+++ editor/gsmwidget.cpp
@@ -0,0 +1,86 @@
+/*
+    Copyright 2013 Lukas Tinkl <ltinkl@redhat.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <KLocalizedString>
+
+#include <QtNetworkManager/settings/gsm.h>
+
+#include "gsmwidget.h"
+#include "ui_gsm.h"
+
+
+GsmWidget::GsmWidget(NetworkManager::Settings::Setting * setting, QWidget* parent, Qt::WindowFlags f):
+    SettingWidget(setting, parent, f),
+    m_ui(new Ui::GsmWidget)
+{
+    m_ui->setupUi(this);
+
+    m_ui->type->addItem(i18nc("GSM network type", "Any"), NetworkManager::Settings::GsmSetting::Any);
+    m_ui->type->addItem(i18n("3G Only (UMTS/HSPA)"), NetworkManager::Settings::GsmSetting::Only3G);
+    m_ui->type->addItem(i18n("2G Only (GPRS/EDGE)"), NetworkManager::Settings::GsmSetting::GprsEdgeOnly);
+    m_ui->type->addItem(i18n("Prefer 3G (UMTS/HSPA)"), NetworkManager::Settings::GsmSetting::Prefer3G);
+    m_ui->type->addItem(i18n("Prefer 2G (GPRS/EDGE)"), NetworkManager::Settings::GsmSetting::Prefer2G);
+    m_ui->type->addItem(i18n("Prefer 4G (LTE)"), NetworkManager::Settings::GsmSetting::Prefer4GLte);
+    m_ui->type->addItem(i18n("4G Only (LTE)"), NetworkManager::Settings::GsmSetting::Only4GLte);
+
+    if (setting)
+        loadConfig(setting);
+}
+
+GsmWidget::~GsmWidget()
+{
+}
+
+void GsmWidget::loadConfig(NetworkManager::Settings::Setting *setting)
+{
+    NetworkManager::Settings::GsmSetting * gsmSetting = static_cast<NetworkManager::Settings::GsmSetting *>(setting);
+    m_ui->number->setText(gsmSetting->number());
+    m_ui->username->setText(gsmSetting->username());
+    m_ui->password->setText(gsmSetting->password());
+
+    m_ui->apn->setText(gsmSetting->apn());
+    m_ui->networkId->setText(gsmSetting->networkId());
+    if (gsmSetting->networkType() != NetworkManager::Settings::GsmSetting::Any)
+        m_ui->type->setCurrentIndex(m_ui->type->findData(static_cast<int>(gsmSetting->networkType())));
+    m_ui->roaming->setChecked(!gsmSetting->homeOnly());
+    m_ui->pin->setText(gsmSetting->pin());
+}
+
+QVariantMap GsmWidget::setting() const
+{
+    NetworkManager::Settings::GsmSetting gsmSetting;
+    if (!m_ui->number->text().isEmpty())
+        gsmSetting.setNumber(m_ui->number->text());
+    if (!m_ui->username->text().isEmpty())
+        gsmSetting.setUsername(m_ui->username->text());
+    if (!m_ui->password->text().isEmpty())
+        gsmSetting.setPassword(m_ui->password->text());
+
+    if (!m_ui->apn->text().isEmpty())
+        gsmSetting.setApn(m_ui->apn->text());
+    if (!m_ui->networkId->text().isEmpty())
+        gsmSetting.setNetworkId(m_ui->networkId->text());
+    gsmSetting.setNetworkType(static_cast<NetworkManager::Settings::GsmSetting::NetworkType>(m_ui->type->itemData(m_ui->type->currentIndex()).toInt()));
+    gsmSetting.setHomeOnly(!m_ui->roaming->isChecked());
+    if (!m_ui->pin->text().isEmpty())
+        gsmSetting.setPin(m_ui->pin->text());
+
+    return gsmSetting.toMap();
+}

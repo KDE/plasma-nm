faeb2c77877b31c0c8663425ed7919006f82b418
Script Kiddy <scripty@kde.org>
2013-03-26 17:03:49 +0100

    SVN_SILENT made messages (.desktop file)

--- applet/declarative/metadata.desktop.cmake
+++ applet/declarative/metadata.desktop.cmake
@@ -1,5 +1,7 @@
 [Desktop Entry]
 Name=Plasma applet for NetworkManager
+Name[cs]=Aplet Plasma pro NetworkManager
+Name[nl]=Plasma-applet voor NetworkManager
 Name[pt]='Applet' do Plasma para o NetworkManager
 Name[pt_BR]=Miniaplicativo do Plasma para o NetworkManager
 Name[sk]=Plasma applet pre NetworkManager
@@ -8,6 +10,8 @@ Name[uk]=Аплет Плазми для NetworkManager
 Name[x-test]=xxPlasma applet for NetworkManagerxx
 
 Comment=Plasma applet written in QML for managing network connections
+Comment[cs]=Aplet Plasma napsaný v QML pro správu síťových spojení
+Comment[nl]=Plasma-applet geschreven in QML voor beheer van netwerkverbindingen
 Comment[pt]=Uma 'applet' do Plasma, feita em QML, para gerir as ligações de rede
 Comment[pt_BR]=Miniaplicativo do Plasma em QML para gerenciamento de conexões de rede
 Comment[sk]=Plasma applet napísaný v QML pre správu sieťových pripojení

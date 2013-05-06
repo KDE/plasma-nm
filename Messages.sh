6ce4a9f93753482f7435b5beee2c488ec5b18124
Burkhard Lück <lueck@hube-lueck.de>
2013-05-06 11:07:36 +0200

    add missing space in Messages.sh

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 $EXTRACTRC `find declarative-plugins -name '*.ui' -o -name '*.rc'` `find settings/details -name '*.ui' -o -name '*.rc'`>> rc.cpp
-$XGETTEXT `find applet -name '*.cpp'` `find declarative-plugins -name '*.cpp'` `find lib -name '*.cpp'` `find settings/details -name '*.cpp'`-o $podir/plasma_applet_org.kde.plasma-nm.pot
+$XGETTEXT `find applet -name '*.cpp'` `find declarative-plugins -name '*.cpp'` `find lib -name '*.cpp'` `find settings/details -name '*.cpp'` -o $podir/plasma_applet_org.kde.plasma-nm.pot
 $XGETTEXT `find applet -name '*.qml'` -j -L Java -o $podir/plasma_applet_org.kde.plasma-nm.pot
 rm -f rc.cpp

6dae4199c89ae9900d223fb718f895965fdcaafb
Lukáš Tinkl <lukas@kde.org>
2013-05-10 15:33:06 +0200

    krazy fix

--- declarative-plugins/applet/networkstatus.cpp
+++ declarative-plugins/applet/networkstatus.cpp
@@ -158,9 +158,9 @@ void NetworkStatus::changeTooltip()
                     conType = UiUtils::interfaceTypeLabel(device->type(), device);
                 }
                 if (active->state() == NetworkManager::ActiveConnection::Activated) {
-                    status = i18n("Connected to %1").arg(active->connection()->name());
+                    status = i18n("Connected to %1", active->connection()->name());
                 } else if (active->state() == NetworkManager::ActiveConnection::Activating) {
-                    status = i18n("Connecting to %1").arg(active->connection()->name());
+                    status = i18n("Connecting to %1", active->connection()->name());
                 }
                 if (active->default4() || active->default6()) {
                     tooltip += QString(formatDefault).arg(devName, conType, status);

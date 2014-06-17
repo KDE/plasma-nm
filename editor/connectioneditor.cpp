44d61e20709d480cc376aab8825d8dfca9b1ab7c
Jan Grulich <jgrulich@redhat.com>
2014-06-17 15:22:09 +0200

    Bigger default size for the editor

--- editor/connectioneditor.cpp
+++ editor/connectioneditor.cpp
@@ -198,7 +198,7 @@ void ConnectionEditor::initializeMenu()
     KStandardAction::keyBindings(guiFactory(), SLOT(configureShortcuts()), actionCollection());
     KStandardAction::quit(this, SLOT(close()), actionCollection());
 
-    createGUI();
+    setupGUI(QSize(480, 480));
 
     setAutoSaveSettings();
 

8c23ab76598a6734cc7e20e4298fe52975f0b802
Jan Grulich <jgrulich@redhat.com>
2014-06-17 15:38:05 +0200

    Do not extract strings from removed code

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-$EXTRACTRC `find settings/notifications -name '*.ui' -o -name '*.rc'` `find applet -name '*.ui'` >> rc.cpp
-$XGETTEXT rc.cpp `find applet -name '*.cpp'` `find libs -name '*.cpp' | grep -v 'libs/editor'` `find settings/notifications -name '*.cpp'` -o $podir/plasma_applet_org.kde.plasma.networkmanagement.pot
+$EXTRACTRC `find applet -name '*.ui'` >> rc.cpp
+$XGETTEXT rc.cpp `find applet -name '*.cpp'` `find libs -name '*.cpp' | grep -v 'libs/editor'` -o $podir/plasma_applet_org.kde.plasma.networkmanagement.pot
 $XGETTEXT `find applet -name '*.qml'` -j -L Java -o $podir/plasma_applet_org.kde.plasma.networkmanagement.pot
 rm -f rc.cpp

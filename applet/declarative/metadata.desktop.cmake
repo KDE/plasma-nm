bbdd00be4a0099d22540c43f1824859c5c2828b1
l10n daemon script <scripty@kde.org>
2014-06-21 04:25:52 +0000

    SVN_SILENT made messages (.desktop file)

--- applet/declarative/metadata.desktop.cmake
+++ applet/declarative/metadata.desktop.cmake
@@ -4,7 +4,6 @@ Name[bg]=Управление на мрежа
 Name[bs]=Upravljanje mrežom
 Name[ca]=Gestió de la xarxa
 Name[ca@valencia]=Gestió de la xarxa
-Name[cs]=Správa sítě
 Name[da]=Netværkshåndtering
 Name[de]=Netzwerkverwaltung
 Name[el]=Διαχείριση δικτύων

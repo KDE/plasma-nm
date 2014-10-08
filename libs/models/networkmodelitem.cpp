c293ea879cb03ebc1ee3792da5c4b92d6e82313d
Jan Grulich <jgrulich@redhat.com>
2014-10-08 10:32:27 +0200

    Do not insert empty VPN banner detail

--- libs/models/networkmodelitem.cpp
+++ libs/models/networkmodelitem.cpp
@@ -535,7 +535,7 @@ void NetworkModelItem::updateDetails()
                 vpnConnection = NetworkManager::VpnConnection::Ptr(new NetworkManager::VpnConnection(active->path()), &QObject::deleteLater);
             }
 
-            if (vpnConnection) {
+            if (vpnConnection && !vpnConnection->banner().isEmpty()) {
                 m_details << i18n("Banner") << vpnConnection->banner().simplified();
             }
         }

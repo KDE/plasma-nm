74cd1d546322d8e958d630631be6f5fa37510bff
Jan Grulich <jgrulich@redhat.com>
2014-11-26 11:17:36 +0100

    Disable debug again This was not supposed to be pushed

--- libs/models/networkmodel.cpp
+++ libs/models/networkmodel.cpp
@@ -33,7 +33,7 @@
 NetworkModel::NetworkModel(QObject* parent)
     : QAbstractListModel(parent)
 {
-    QLoggingCategory::setFilterRules(QStringLiteral("plasma-nm.debug = true"));
+    QLoggingCategory::setFilterRules(QStringLiteral("plasma-nm.debug = false"));
 
     initialize();
 }

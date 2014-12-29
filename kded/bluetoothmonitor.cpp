a1f4bcfa47a39e0c221419a9d6fb235946223084
Alex Richardson <arichardson.kde@gmail.com>
2014-12-29 17:24:39 +0100

    fix build if WITH_MODEMMANAGER_SUPPORT is 0

--- kded/bluetoothmonitor.cpp
+++ kded/bluetoothmonitor.cpp
@@ -241,8 +241,8 @@ void BluetoothMonitor::addBluetoothConnection(const QString& bdAddr, const QStri
                                 delete mobileConnectionWizard;
                             }
                         }
-                    }
 #endif
+                    }
                 });
             });
         }

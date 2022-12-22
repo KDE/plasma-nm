KDE/plasma-nm

commit 408cca64874fee63dd3b5550e4d2bbeb8944229b

Add support for openconnect_set_external_browser_callback introduced in openconnect v9.0 (libopenconnect 5.8) using QDesktopServices
Default libopenconnect external browser callback leads to zombie processes that
cause plasma-nm flows to break under certain conditions related to the external
browser process. Delegate showing the login URI to QDesktopServices.

Signed-off-by: Rahul Rameshbabu <sergeantsagara@protonmail.com>

vpn/openconnect/openconnectauthworkerthread.cpp
@@ -11,6 +11,11 @@
 #include <QString>
 #include <QWaitCondition>
 
+#if OPENCONNECT_CHECK_VER(5, 8)
+#include <QUrl>
+#include <QDesktopServices>
+#endif
+
 extern "C" {
 #include <cerrno>
 #include <cstdlib>
@@ -21,6 +26,14 @@ extern "C" {
 class OpenconnectAuthStaticWrapper
 {
 public:
+#if OPENCONNECT_CHECK_VER(5, 8)
+    static int openUri(struct openconnect_info *vpninfo, const char *login_uri, void *obj)
+    {
+        if (obj)
+            return static_cast<OpenconnectAuthWorkerThread *>(obj)->openUri(vpninfo, login_uri, obj);
+        return -1;
+    }
+#endif
 #if OPENCONNECT_CHECK_VER(5, 0)
     static int writeNewConfig(void *obj, const char *str, int num)
     {
@@ -83,6 +96,9 @@ OpenconnectAuthWorkerThread::OpenconnectAuthWorkerThread(QMutex *mutex,
                                                 OpenconnectAuthStaticWrapper::writeProgress,
                                                 this);
     openconnect_set_cancel_fd(m_openconnectInfo, cancelFd);
+#if OPENCONNECT_CHECK_VER(5, 8)
+    openconnect_set_external_browser_callback(m_openconnectInfo, OpenconnectAuthStaticWrapper::openUri);
+#endif
 }
 
 OpenconnectAuthWorkerThread::~OpenconnectAuthWorkerThread()
@@ -186,3 +202,17 @@ void OpenconnectAuthWorkerThread::writeProgress(int level, const char *fmt, va_l
     const QString msg = QString::vasprintf(fmt, argPtr);
     Q_EMIT updateLog(msg, level);
 }
+
+#if OPENCONNECT_CHECK_VER(5, 8)
+int OpenconnectAuthWorkerThread::openUri(__attribute__((unused)) struct openconnect_info *vpninfo,
+                                         const char *login_uri, __attribute__((unused)) void *privdata)
+{
+    bool opened = QDesktopServices::openUrl(QUrl(login_uri, QUrl::TolerantMode));
+    if (!opened) {
+        OpenconnectAuthWorkerThread::writeProgress(PRG_ERR, "Failed to invoke QDesktopServices::openUrl.", nullptr);
+        return 1;
+    }
+
+    return 0;
+}
+#endif

CMakeLists.txt
@@ -30,6 +30,7 @@ include(FeatureSummary)
 find_package(Qt${QT_MAJOR_VERSION} ${QT_MIN_VERSION} CONFIG REQUIRED COMPONENTS
     Core
     DBus
+    Gui
     Network
     Quick
     QuickWidgets

vpn/openconnect/openconnectauthworkerthread.h
@@ -75,6 +75,9 @@ private:
     int validatePeerCert(void *, const char *);
     int processAuthFormP(struct oc_auth_form *);
     void writeProgress(int level, const char *, va_list);
+#if OPENCONNECT_CHECK_VER(5, 8)
+    int openUri(struct openconnect_info *, const char *, void *);
+#endif
 
     QMutex *m_mutex;
     QWaitCondition *m_waitForUserInput;

commit 4654501e5e4321cffbcb88e28b1411cc4b975bac

Remove UB parameter passing to va_list argument in OpenconnectAuthWorkerThread::openUri
OpenconnectAuthWorkerThread::openUri previously passed a UB nullptr argument to
OpenconnectAuthWorkerThread::writeProgress. This can potential break builds for
certain target ABIs. Similarly, passing a va_list that was not initialized with
va_start/va_copy would be UB as well. This patch omits needing interact with the
va_list argument by calling OpenconnectAuthStaticWrapper::writeProgress instead.

Fixes: 408cca64874f ("Add support for openconnect_set_external_browser_callback introduced in openconnect v9.0 (libopenconnect 5.8) using QDesktopServices")
Signed-off-by: Rahul Rameshbabu <sergeantsagara@protonmail.com>

vpn/openconnect/openconnectauthworkerthread.cpp
@@ -209,7 +209,7 @@ int OpenconnectAuthWorkerThread::openUri(__attribute__((unused)) struct openconn
 {
     bool opened = QDesktopServices::openUrl(QUrl(login_uri, QUrl::TolerantMode));
     if (!opened) {
-        OpenconnectAuthWorkerThread::writeProgress(PRG_ERR, "Failed to invoke QDesktopServices::openUrl.", nullptr);
+        OpenconnectAuthStaticWrapper::writeProgress(this, PRG_ERR, "Failed to invoke QDesktopServices::openUrl.");
         return 1;
     }
 
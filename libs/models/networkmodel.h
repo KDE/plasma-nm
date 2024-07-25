73683eae84f7864b88d8601a3592671857e4bd13
Kai Uwe Broulik <kde@privat.broulik.de>
2024-07-25 17:09:38 +0200

    NetworkModel: Add default argument to rowCount() and data()
    Matches QAbstractItemModel but also makes it easier to
    call it from QML since you can just call rowCount()
    without having to worry about the QModelIndex, it's not
    a tree model after all.

--- libs/models/networkmodel.h
+++ libs/models/networkmodel.h
@@ -72,8 +72,8 @@ public:
 
     enum ModelChangeType { ItemAdded, ItemRemoved, ItemPropertyChanged };
 
-    int rowCount(const QModelIndex &parent) const override;
-    QVariant data(const QModelIndex &index, int role) const override;
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
     bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
     QHash<int, QByteArray> roleNames() const override;
 

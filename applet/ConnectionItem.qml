KDE/plasma-nm

commit 09569fa7c30b5e61493f1f126580550093c00eb2

wifi: refactor security UI layout and 802.1x form behavior

applet/ConnectionItem.qml
@@ -25,7 +25,7 @@ PlasmaExtras.ExpandableListItem {
 
     required property var model
     required property var listView
-    required index
+    required property int index
 
     property bool activating: model.ConnectionState === PlasmaNM.Enums.Activating
     property bool deactivated: model.ConnectionState === PlasmaNM.Enums.Deactivated

libs/editor/connectioneditorbase.cpp
@@ -48,6 +48,9 @@
 #include <KNotification>
 #include <KUser>
 
+#include <QHBoxLayout>
+#include <QScrollArea>
+
 ConnectionEditorBase::ConnectionEditorBase(const NetworkManager::ConnectionSettings::Ptr &connection, QWidget *parent, Qt::WindowFlags f)
     : QWidget(parent, f)
     , m_initialized(false)
@@ -181,7 +184,7 @@ void ConnectionEditorBase::addConnectionWidget(ConnectionWidget *widget, const Q
 
     connect(widget, &ConnectionWidget::settingChanged, this, &ConnectionEditorBase::settingChanged);
 
-    addWidget(widget, text);
+    addWidget(wrapWithPreferredWidth(widget), text);
 }
 
 void ConnectionEditorBase::addSettingWidget(SettingWidget *widget, const QString &text)
@@ -190,7 +193,7 @@ void ConnectionEditorBase::addSettingWidget(SettingWidget *widget, const QString
 
     connect(widget, &SettingWidget::settingChanged, this, &ConnectionEditorBase::settingChanged);
 
-    addWidget(widget, text);
+    addWidget(wrapWithPreferredWidth(widget), text);
 }
 
 void ConnectionEditorBase::initialize()
@@ -203,7 +206,7 @@ void ConnectionEditorBase::initialize()
     } else {
         // Status tab showing connection details (first tab)
         m_statusWidget = new ConnectionStatusWidget(m_connection->uuid());
-        addWidget(m_statusWidget, i18nc("@title:tab Connection status and details", "Status"));
+        addWidget(wrapWithPreferredWidth(m_statusWidget), i18nc("@title:tab Connection status and details", "Status"));
         updateStatusWidget();
     }
 
@@ -225,7 +228,18 @@ void ConnectionEditorBase::initialize()
                                                    m_connection->setting(NetworkManager::Setting::WirelessSecurity),
                                                    m_connection->setting(NetworkManager::Setting::Security8021x).staticCast<NetworkManager::Security8021xSetting>(),
                                                    this);
-        addSettingWidget(wifiWidget, i18n("Wi-Fi"));
+        m_settingWidgets << wifiWidget;
+        connect(wifiWidget, &SettingWidget::settingChanged, this, &ConnectionEditorBase::settingChanged);
+
+        // Wrap in scroll area for vertical scrolling, allow horizontal when needed
+        auto scrollArea = new QScrollArea(this);
+        scrollArea->setWidget(wifiWidget);
+        scrollArea->setWidgetResizable(true);
+        scrollArea->setFrameShape(QFrame::NoFrame);
+        scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+        scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+
+        addWidget(wrapWithPreferredWidth(scrollArea), i18n("Wi-Fi"));
         m_wifiConnectionWidget = wifiWidget;
     } else if (type == NetworkManager::ConnectionSettings::Pppoe) { // DSL
         auto pppoeWidget = new PppoeWidget(m_connection->setting(NetworkManager::Setting::Pppoe), this);
@@ -539,12 +553,17 @@ void ConnectionEditorBase::validChanged(bool valid)
 
 void ConnectionEditorBase::onAllUsersChanged()
 {
-    if (!m_wifiConnectionWidget || !m_wifiConnectionWidget->wifiSecurity()) {
+    if (!m_wifiConnectionWidget) {
+        return;
+    }
+
+    WifiSecurity *wifiSecurity = m_wifiConnectionWidget->wifiSecurity();
+    if (!wifiSecurity) {
         return;
     }
 
     auto allUsers = m_connectionWidget->allUsers();
-    m_wifiConnectionWidget->wifiSecurity()->setStoreSecretsSystemWide(allUsers);
+    wifiSecurity->setStoreSecretsSystemWide(allUsers);
 }
 
 void ConnectionEditorBase::updateStatusWidget()
@@ -645,6 +664,28 @@ void ConnectionEditorBase::updateStatusWidget()
 
     // Pass connection, device, and access point path to status widget
     m_statusWidget->setConnectionAndDevice(nmConnection, device, accessPointPath);
+}
+
+QWidget *ConnectionEditorBase::wrapWithPreferredWidth(QWidget *content)
+{
+    auto *container = new QWidget(this);
+    auto *layout = new QHBoxLayout(container);
+    layout->setContentsMargins(0, 0, 0, 0);
+
+    layout->addStretch(1);
+
+    // Set size constraints on content:
+    // - Preferred width: 600px for typical English labels
+    // - Maximum width: 800px to handle longer labels in other languages
+    // - Content can grow between preferred and max as needed
+    content->setMinimumWidth(TAB_CONTENT_PREFERRED_WIDTH);
+    content->setMaximumWidth(TAB_CONTENT_MAX_WIDTH);
+    content->setParent(container);
+    layout->addWidget(content);
+
+    layout->addStretch(1);
+
+    return container;
 }
 
 #include "moc_connectioneditorbase.cpp"

libs/editor/connectioneditorbase.h
@@ -75,6 +75,10 @@ private:
     void addConnectionWidget(ConnectionWidget *widget, const QString &text);
     void addSettingWidget(SettingWidget *widget, const QString &text);
     void updateStatusWidget();
+    QWidget *wrapWithPreferredWidth(QWidget *content);
+
+    static constexpr int TAB_CONTENT_PREFERRED_WIDTH = 600;
+    static constexpr int TAB_CONTENT_MAX_WIDTH = 800;
 };
 
 #endif // PLASMA_NM_CONNECTION_EDITOR_BASE_H

libs/editor/settings/security802-1x.cpp
@@ -12,6 +12,7 @@
 #include <KAcceleratorManager>
 #include <KLocalizedString>
 
+#include <QFormLayout>
 #include <QtCrypto>
 
 Security8021x::Security8021x(const NetworkManager::Setting::Ptr &setting, Type type, QWidget *parent, Qt::WindowFlags f)
@@ -31,7 +32,6 @@ Security8021x::Security8021x(const NetworkManager::Setting::Ptr &setting, Type t
 
     if (type == WirelessWpaEap) {
         m_ui->auth->removeItem(0); // MD 5
-        m_ui->stackedWidget->removeWidget(m_ui->md5Page);
 
         m_ui->auth->setItemData(0, NetworkManager::Security8021xSetting::EapMethodTls);
         m_ui->auth->setItemData(1, NetworkManager::Security8021xSetting::EapMethodLeap);
@@ -47,17 +47,10 @@ Security8021x::Security8021x(const NetworkManager::Setting::Ptr &setting, Type t
         m_ui->auth->removeItem(3); // PWD
         m_ui->auth->removeItem(2); // LEAP
         m_ui->auth->removeItem(0); // MD5
-        m_ui->stackedWidget->removeWidget(m_ui->peapPage);
-        m_ui->stackedWidget->removeWidget(m_ui->ttlsPage);
-        m_ui->stackedWidget->removeWidget(m_ui->fastPage);
-        m_ui->stackedWidget->removeWidget(m_ui->pwdPage);
-        m_ui->stackedWidget->removeWidget(m_ui->leapPage);
-        m_ui->stackedWidget->removeWidget(m_ui->md5Page);
 
         m_ui->auth->setItemData(0, NetworkManager::Security8021xSetting::EapMethodTls);
     } else {
         m_ui->auth->removeItem(2); // LEAP
-        m_ui->stackedWidget->removeWidget(m_ui->leapPage);
 
         m_ui->auth->setItemData(0, NetworkManager::Security8021xSetting::EapMethodMd5);
         m_ui->auth->setItemData(1, NetworkManager::Security8021xSetting::EapMethodTls);
@@ -112,7 +105,7 @@ Security8021x::Security8021x(const NetworkManager::Setting::Ptr &setting, Type t
     connect(m_ui->peapPassword, &PasswordField::passwordOptionChanged, this, &Security8021x::slotWidgetChanged);
 
     KAcceleratorManager::manage(this);
-    connect(m_ui->stackedWidget, &QStackedWidget::currentChanged, this, &Security8021x::currentAuthChanged);
+    connect(m_ui->auth, QOverload<int>::of(&KComboBox::currentIndexChanged), this, &Security8021x::currentAuthChanged);
 
     altSubjectValidator = new QRegularExpressionValidator(
         QRegularExpression(
@@ -127,6 +120,9 @@ Security8021x::Security8021x(const NetworkManager::Setting::Ptr &setting, Type t
     auto serverListValidator = new ListValidator(this);
     serverListValidator->setInnerValidator(serversValidator);
     m_ui->leTlsConnectToServers->setValidator(serverListValidator);
+
+    // Initialize row visibility based on default authentication method
+    currentAuthChanged(0);
 
     if (setting) {
         loadConfig(setting);
@@ -609,7 +605,101 @@ bool Security8021x::isValid() const
 void Security8021x::currentAuthChanged(int index)
 {
     Q_UNUSED(index);
-    KAcceleratorManager::manage(m_ui->stackedWidget->currentWidget());
+
+    // Hide all authentication-specific fields first
+    // MD5
+    m_ui->formLayout_7->setRowVisible(m_ui->md5UserName, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->md5Password, false);
+
+    // TLS
+    m_ui->formLayout_7->setRowVisible(m_ui->tlsIdentity, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->tlsDomain, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->tlsUserCert, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->tlsCACert, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->tlsPrivateKey, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->tlsPrivateKeyPassword, false);
+
+    // LEAP
+    m_ui->formLayout_7->setRowVisible(m_ui->leapUsername, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->leapPassword, false);
+
+    // PWD
+    m_ui->formLayout_7->setRowVisible(m_ui->pwdUsername, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->pwdPassword, false);
+
+    // FAST
+    m_ui->formLayout_7->setRowVisible(m_ui->fastAnonIdentity, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->fastAllowPacProvisioning, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->pacFile, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->fastInnerAuth, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->fastUsername, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->fastPassword, false);
+
+    // TTLS
+    m_ui->formLayout_7->setRowVisible(m_ui->ttlsAnonIdentity, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->ttlsCACert, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->ttlsInnerAuth, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->ttlsUsername, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->ttlsPassword, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->ttlsDomain, false);
+
+    // PEAP
+    m_ui->formLayout_7->setRowVisible(m_ui->peapAnonIdentity, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->peapCACert, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->peapVersion, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->peapInnerAuth, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->peapUsername, false);
+    m_ui->formLayout_7->setRowVisible(m_ui->peapPassword, false);
+
+    // Show fields for selected authentication method
+    const int authIndex = m_ui->auth->currentData().toInt();
+
+    switch (authIndex) {
+    case NetworkManager::Security8021xSetting::EapMethodMd5:
+        m_ui->formLayout_7->setRowVisible(m_ui->md5UserName, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->md5Password, true);
+        break;
+    case NetworkManager::Security8021xSetting::EapMethodTls:
+        m_ui->formLayout_7->setRowVisible(m_ui->tlsIdentity, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->tlsDomain, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->tlsUserCert, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->tlsCACert, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->tlsPrivateKey, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->tlsPrivateKeyPassword, true);
+        break;
+    case NetworkManager::Security8021xSetting::EapMethodLeap:
+        m_ui->formLayout_7->setRowVisible(m_ui->leapUsername, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->leapPassword, true);
+        break;
+    case NetworkManager::Security8021xSetting::EapMethodPwd:
+        m_ui->formLayout_7->setRowVisible(m_ui->pwdUsername, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->pwdPassword, true);
+        break;
+    case NetworkManager::Security8021xSetting::EapMethodFast:
+        m_ui->formLayout_7->setRowVisible(m_ui->fastAnonIdentity, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->fastAllowPacProvisioning, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->pacFile, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->fastInnerAuth, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->fastUsername, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->fastPassword, true);
+        break;
+    case NetworkManager::Security8021xSetting::EapMethodTtls:
+        m_ui->formLayout_7->setRowVisible(m_ui->ttlsAnonIdentity, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->ttlsCACert, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->ttlsInnerAuth, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->ttlsUsername, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->ttlsPassword, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->ttlsDomain, true);
+        break;
+    case NetworkManager::Security8021xSetting::EapMethodPeap:
+        m_ui->formLayout_7->setRowVisible(m_ui->peapAnonIdentity, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->peapCACert, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->peapVersion, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->peapInnerAuth, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->peapUsername, true);
+        m_ui->formLayout_7->setRowVisible(m_ui->peapPassword, true);
+        break;
+    }
 }
 
 #include "moc_security802-1x.cpp"

libs/editor/settings/ui/wificonnectionwidget.ui
@@ -10,8 +10,30 @@
     <height>334</height>
    </rect>
   </property>
+  <property name="minimumSize">
+   <size>
+    <width>250</width>
+    <height>0</height>
+   </size>
+  </property>
   <layout class="QFormLayout" name="formLayout">
-   <item row="0" column="0">
+   <item row="0" column="0" colspan="2">
+    <widget class="QLabel" name="connectionSectionLabel">
+     <property name="text">
+      <string>Connection</string>
+     </property>
+     <property name="font">
+      <font>
+       <weight>75</weight>
+       <bold>true</bold>
+      </font>
+     </property>
+     <property name="alignment">
+      <set>Qt::AlignCenter</set>
+     </property>
+    </widget>
+   </item>
+   <item row="1" column="0">
     <widget class="QLabel" name="SSIDLabel">
      <property name="text">
       <string>SSID:</string>
@@ -24,7 +46,7 @@
      </property>
     </widget>
    </item>
-   <item row="0" column="1">
+   <item row="1" column="1">
     <widget class="SsidComboBox" name="SSIDCombo">
      <property name="sizePolicy">
       <sizepolicy hsizetype="MinimumExpanding" vsizetype="Fixed">
@@ -40,7 +62,7 @@
      </property>
     </widget>
    </item>
-   <item row="1" column="0">
+   <item row="2" column="0">
     <widget class="QLabel" name="modeLabel">
      <property name="text">
       <string>Mode:</string>
@@ -53,7 +75,7 @@
      </property>
     </widget>
    </item>
-   <item row="1" column="1">
+   <item row="2" column="1">
     <widget class="KComboBox" name="modeComboBox">
      <property name="sizePolicy">
       <sizepolicy hsizetype="MinimumExpanding" vsizetype="Fixed">
@@ -84,7 +106,23 @@
      </item>
     </widget>
    </item>
-   <item row="2" column="0">
+   <item row="3" column="0" colspan="2">
+    <widget class="QLabel" name="advancedSectionLabel">
+     <property name="text">
+      <string>Advanced</string>
+     </property>
+     <property name="font">
+      <font>
+       <weight>75</weight>
+       <bold>true</bold>
+      </font>
+     </property>
+     <property name="alignment">
+      <set>Qt::AlignCenter</set>
+     </property>
+    </widget>
+   </item>
+   <item row="4" column="0">
     <widget class="QLabel" name="BSSIDLabel">
      <property name="text">
       <string>Restrict to BSSID:</string>
@@ -97,7 +135,7 @@
      </property>
     </widget>
    </item>
-   <item row="2" column="1">
+   <item row="4" column="1">
     <widget class="BssidComboBox" name="BSSIDCombo">
      <property name="sizePolicy">
       <sizepolicy hsizetype="MinimumExpanding" vsizetype="Fixed">
@@ -113,7 +151,7 @@
      </property>
     </widget>
    </item>
-   <item row="3" column="0">
+   <item row="5" column="0">
     <widget class="QLabel" name="bandLabel">
      <property name="sizePolicy">
       <sizepolicy hsizetype="Preferred" vsizetype="Minimum">
@@ -132,7 +170,7 @@
      </property>
     </widget>
    </item>
-   <item row="3" column="1">
+   <item row="5" column="1">
     <widget class="KComboBox" name="band">
      <property name="sizePolicy">
       <sizepolicy hsizetype="MinimumExpanding" vsizetype="Minimum">
@@ -161,7 +199,7 @@
      </item>
     </widget>
    </item>
-   <item row="4" column="0">
+   <item row="6" column="0">
     <widget class="QLabel" name="channelLabel">
      <property name="text">
       <string>Channel:</string>
@@ -174,7 +212,7 @@
      </property>
     </widget>
    </item>
-   <item row="4" column="1">
+   <item row="6" column="1">
     <widget class="KComboBox" name="channel">
      <property name="sizePolicy">
       <sizepolicy hsizetype="MinimumExpanding" vsizetype="Fixed">
@@ -188,7 +226,7 @@ a Wi-Fi network on the specified channel</string>
      </property>
     </widget>
    </item>
-   <item row="5" column="0">
+   <item row="7" column="0">
     <widget class="QLabel" name="label">
      <property name="text">
       <string>Restrict to network adapter:</string>
@@ -201,7 +239,7 @@ a Wi-Fi network on the specified channel</string>
      </property>
     </widget>
    </item>
-   <item row="5" column="1">
+   <item row="7" column="1">
     <widget class="HwAddrComboBox" name="macAddress">
      <property name="sizePolicy">
       <sizepolicy hsizetype="MinimumExpanding" vsizetype="Fixed">
@@ -214,7 +252,7 @@ a Wi-Fi network on the specified channel</string>
      </property>
     </widget>
    </item>
-   <item row="6" column="0">
+   <item row="8" column="0">
     <widget class="QLabel" name="label_2">
      <property name="text">
       <string>Cloned MAC address:</string>
@@ -227,7 +265,7 @@ a Wi-Fi network on the specified channel</string>
      </property>
     </widget>
    </item>
-   <item row="6" column="1">
+   <item row="8" column="1">
     <layout class="QHBoxLayout" name="horizontalLayout">
      <item>
       <widget class="KLineEdit" name="clonedMacAddress">
@@ -257,7 +295,7 @@ a Wi-Fi network on the specified channel</string>
      </item>
     </layout>
    </item>
-   <item row="7" column="0">
+   <item row="9" column="0">
     <widget class="QLabel" name="label_3">
      <property name="text">
       <string>MTU:</string>
@@ -270,7 +308,7 @@ a Wi-Fi network on the specified channel</string>
      </property>
     </widget>
    </item>
-   <item row="7" column="1">
+   <item row="9" column="1">
     <widget class="QSpinBox" name="mtu">
      <property name="sizePolicy">
       <sizepolicy hsizetype="MinimumExpanding" vsizetype="Fixed">
@@ -298,7 +336,7 @@ a Wi-Fi network on the specified channel</string>
      </property>
     </widget>
    </item>
-   <item row="8" column="0">
+   <item row="10" column="0">
     <widget class="QLabel" name="label_4">
      <property name="toolTip">
       <string>Create a connection for a hidden network</string>
@@ -314,7 +352,7 @@ a Wi-Fi network on the specified channel</string>
      </property>
     </widget>
    </item>
-   <item row="8" column="1">
+   <item row="10" column="1">
     <widget class="QCheckBox" name="hiddenNetwork">
      <property name="toolTip">
       <string>Create a connection for a hidden network</string>
@@ -324,18 +362,21 @@ a Wi-Fi network on the specified channel</string>
      </property>
     </widget>
    </item>
-   <item row="9" column="0">
-    <spacer name="verticalSpacer">
-     <property name="orientation">
-      <enum>Qt::Vertical</enum>
+   <item row="11" column="0" colspan="2">
+    <widget class="QLabel" name="securitySectionLabel">
+     <property name="text">
+      <string>Security</string>
      </property>
-     <property name="sizeHint" stdset="0">
-      <size>
-       <width>20</width>
-       <height>40</height>
-      </size>
+     <property name="font">
+      <font>
+       <weight>75</weight>
+       <bold>true</bold>
+      </font>
      </property>
-    </spacer>
+     <property name="alignment">
+      <set>Qt::AlignCenter</set>
+     </property>
+    </widget>
    </item>
   </layout>
  </widget>

libs/editor/settings/ui/wifisecurity.ui
@@ -14,13 +14,28 @@
   <property name="windowTitle">
    <string>Wifi Security</string>
   </property>
-  <layout class="QVBoxLayout" name="verticalLayout">
-   <item>
-    <layout class="QFormLayout" name="formLayout">
+  <layout class="QFormLayout" name="formLayout">
+     <property name="leftMargin">
+      <number>0</number>
+     </property>
+     <property name="topMargin">
+      <number>0</number>
+     </property>
+     <property name="rightMargin">
+      <number>0</number>
+     </property>
+     <property name="bottomMargin">
+      <number>0</number>
+     </property>     <property name="labelAlignment">
+      <set>Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter</set>
+     </property>
      <item row="0" column="0">
       <widget class="QLabel" name="label">
        <property name="text">
         <string>Security:</string>
+       </property>
+       <property name="alignment">
+        <set>Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter</set>
        </property>
        <property name="buddy">
         <cstring>securityCombo</cstring>
@@ -92,6 +107,9 @@
        <property name="text">
         <string>Key:</string>
        </property>
+       <property name="alignment">
+        <set>Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter</set>
+       </property>
        <property name="buddy">
         <cstring>wepKey</cstring>
        </property>
@@ -111,6 +129,9 @@
       <widget class="QLabel" name="label_3">
        <property name="text">
         <string>WEP index:</string>
+       </property>
+       <property name="alignment">
+        <set>Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter</set>
        </property>
        <property name="buddy">
         <cstring>wepIndex</cstring>
@@ -152,6 +173,9 @@
        <property name="text">
         <string>Authentication:</string>
        </property>
+       <property name="alignment">
+        <set>Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter</set>
+       </property>
        <property name="buddy">
         <cstring>wepAuth</cstring>
        </property>
@@ -182,6 +206,9 @@
        <property name="text">
         <string>Username:</string>
        </property>
+       <property name="alignment">
+        <set>Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter</set>
+       </property>
        <property name="buddy">
         <cstring>leapUsername</cstring>
        </property>
@@ -194,6 +221,9 @@
       <widget class="QLabel" name="label_9">
        <property name="text">
         <string>Password:</string>
+       </property>
+       <property name="alignment">
+        <set>Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter</set>
        </property>
        <property name="buddy">
         <cstring>leapPassword</cstring>
@@ -212,6 +242,9 @@
        <property name="text">
         <string>Password:</string>
        </property>
+       <property name="alignment">
+        <set>Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter</set>
+       </property>
        <property name="buddy">
         <cstring>psk</cstring>
        </property>
@@ -224,47 +257,6 @@
        </property>
       </widget>
      </item>
-    </layout>
-   </item>
-   <item>
-    <widget class="QWidget" name="stackedWidgetContainer" native="true">
-     <layout class="QVBoxLayout" name="stackedLayout">
-      <property name="leftMargin">
-       <number>0</number>
-      </property>
-      <property name="topMargin">
-       <number>0</number>
-      </property>
-      <property name="rightMargin">
-       <number>0</number>
-      </property>
-      <property name="bottomMargin">
-       <number>0</number>
-      </property>
-      <item>
-       <widget class="QStackedWidget" name="stackedWidget">
-        <property name="currentIndex">
-         <number>0</number>
-        </property>
-        <widget class="QWidget" name="securityNone"/>
-       </widget>
-      </item>
-     </layout>
-    </widget>
-   </item>
-   <item>
-    <spacer name="verticalSpacer">
-     <property name="orientation">
-      <enum>Qt::Vertical</enum>
-     </property>
-     <property name="sizeHint" stdset="0">
-      <size>
-       <width>20</width>
-       <height>40</height>
-      </size>
-     </property>
-    </spacer>
-   </item>
   </layout>
  </widget>
  <customwidgets>

libs/editor/settings/wificonnectionwidget.cpp
@@ -11,7 +11,9 @@
 
 #include <KLocalizedString>
 #include <NetworkManagerQt/Utils>
+#include <QFormLayout>
 #include <QRandomGenerator>
+#include <QTimer>
 
 WifiConnectionWidget::WifiConnectionWidget(const NetworkManager::Setting::Ptr &setting,
                                            const NetworkManager::Setting::Ptr &securitySetting,
@@ -23,9 +25,29 @@ WifiConnectionWidget::WifiConnectionWidget(const NetworkManager::Setting::Ptr &s
 {
     m_ui->setupUi(this);
 
+    // Set font size for section headers to match Status tab style (base font + 1)
+    for (auto sectionLabel : {m_ui->connectionSectionLabel, m_ui->advancedSectionLabel, m_ui->securitySectionLabel}) {
+        QFont font = sectionLabel->font();
+        font.setPointSize(font.pointSize() + 1);
+        sectionLabel->setFont(font);
+    }
+
     // Create and embed the WifiSecurity widget
     m_wifiSecurity = new WifiSecurity(securitySetting, setting8021x, this);
-    m_ui->securityWidgetLayout->addWidget(m_wifiSecurity);
+
+    // Set vertical spacing to match parent for all nested form layouts
+    if (auto parentFormLayout = qobject_cast<QFormLayout *>(layout())) {
+        if (auto securityFormLayout = qobject_cast<QFormLayout *>(m_wifiSecurity->securityLayout())) {
+            securityFormLayout->setVerticalSpacing(parentFormLayout->verticalSpacing());
+        }
+        const auto nestedFormLayouts = m_wifiSecurity->findChildren<QFormLayout *>();
+        for (auto formLayout : nestedFormLayouts) {
+            formLayout->setVerticalSpacing(parentFormLayout->verticalSpacing());
+        }
+
+        // Insert the security widget at row 12 (after the Security header at row 11)
+        parentFormLayout->insertRow(12, m_wifiSecurity);
+    }
 
     // Connect SSID changes to security widget for auto-detection
     connect(this, QOverload<const QString &>::of(&WifiConnectionWidget::ssidChanged), m_wifiSecurity, &WifiSecurity::onSsidChanged);
@@ -51,6 +73,42 @@ WifiConnectionWidget::WifiConnectionWidget(const NetworkManager::Setting::Ptr &s
     if (setting) {
         loadConfig(setting);
     }
+
+    // Synchronize label widths after layouts are populated
+    QTimer::singleShot(0, this, [this]() {
+        QList<QFormLayout *> layouts;
+        if (auto mainLayout = findChild<QFormLayout *>(QStringLiteral("formLayout"))) {
+            layouts.append(mainLayout);
+        }
+        if (m_wifiSecurity) {
+            // Include all form layouts from wifisecurity and nested widgets (e.g., 802-1x)
+            const auto formLayouts = m_wifiSecurity->findChildren<QFormLayout *>();
+            layouts.append(formLayouts);
+        }
+
+        int maxWidth = 0;
+        for (auto layout : layouts) {
+            for (int row = 0; row < layout->rowCount(); ++row) {
+                if (auto item = layout->itemAt(row, QFormLayout::LabelRole)) {
+                    if (auto label = item->widget()) {
+                        maxWidth = std::max(maxWidth, label->sizeHint().width());
+                    }
+                }
+            }
+        }
+
+        if (maxWidth > 0) {
+            for (auto layout : layouts) {
+                for (int row = 0; row < layout->rowCount(); ++row) {
+                    if (auto item = layout->itemAt(row, QFormLayout::LabelRole)) {
+                        if (auto label = item->widget()) {
+                            label->setMinimumWidth(maxWidth);
+                        }
+                    }
+                }
+            }
+        }
+    });
 }
 
 WifiConnectionWidget::~WifiConnectionWidget()

commit 6ddbb900e125c1236934e4aa4cd8b0c85bee3a1e

applet/ConnectionItem: Don't shadow index property (again)
09569fa7c30b5e61493f1f126580550093c00eb2 accidentally reverted
a2afe64e357652b21e7947b59a395d8fd3764f3c, so bug 514034 is back.

ExpandableListItem already has an index property, so we don't need
to redefine it, just mark it as required.

BUG: 514034

applet/ConnectionItem.qml
@@ -25,7 +25,7 @@ PlasmaExtras.ExpandableListItem {
 
     required property var model
     required property var listView
-    required property int index
+    required index
 
     property bool activating: model.ConnectionState === PlasmaNM.Enums.Activating
     property bool deactivated: model.ConnectionState === PlasmaNM.Enums.Deactivated